29e0d0de1dc9f13cd44ad1c1c1c57d424a6d6eaa
bordyale <alessio.bordignon@gmail.com>
2025-01-13 20:16:21 +0100

    upload

--- src/main/java/org/apache/ofbiz/vvorder/VvorderEvents.java
+++ src/main/java/org/apache/ofbiz/vvorder/VvorderEvents.java
@@ -18,9 +18,15 @@
  *******************************************************************************/
 package org.apache.ofbiz.vvorder;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,11 +38,16 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import java.io.RandomAccessFile;
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.file.StandardOpenOption;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.FileUtil;
 import org.apache.ofbiz.base.util.ObjectType;
 import org.apache.ofbiz.base.util.UtilDateTime;
 import org.apache.ofbiz.base.util.UtilFormatOut;
@@ -59,6 +70,9 @@ import org.apache.ofbiz.entity.util.EntityUtil;
 import org.apache.ofbiz.order.shoppingcart.ShoppingCart;
 import org.apache.ofbiz.order.shoppingcart.ShoppingCartEvents;
 import org.apache.ofbiz.order.shoppingcart.product.ProductPromoWorker;
+import org.apache.ofbiz.pricat.AbstractPricatParser;
+import org.apache.ofbiz.pricat.InterfacePricatParser;
+import org.apache.ofbiz.pricat.PricatParseExcelHtmlThread;
 import org.apache.ofbiz.product.catalog.CatalogWorker;
 import org.apache.ofbiz.product.config.ProductConfigWorker;
 import org.apache.ofbiz.product.config.ProductConfigWrapper;
@@ -72,6 +86,10 @@ import org.apache.ofbiz.service.ModelService;
 import org.apache.ofbiz.service.ServiceUtil;
 import org.apache.ofbiz.webapp.control.RequestHandler;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 /**
  * Vforder events.
  */
@@ -248,7 +266,8 @@ public class VvorderEvents {
 					for (GenericValue det : invDetails) {
 						exprs.add(EntityCondition.makeCondition("whId", EntityOperator.EQUALS, det.get("whId")));
 					}
-					//delegator.removeByAnd("VvWarehouse", UtilMisc.toMap("shipmentId", shipmentId));
+					// delegator.removeByAnd("VvWarehouse",
+					// UtilMisc.toMap("shipmentId", shipmentId));
 					EntityConditionList<EntityCondition> ecl = EntityCondition.makeCondition(exprs, EntityOperator.OR);
 					List<GenericValue> inv = EntityQuery.use(delegator).from("VvWarehouse").where(ecl).queryList();
 					delegator.removeAll(inv);
@@ -269,8 +288,8 @@ public class VvorderEvents {
 
 						try {
 							BigDecimal qty = (BigDecimal) si.get("quantity");
-							Map serviceCtx = UtilMisc.toMap("productId", si.get("productId"), "shipmentId", shipmentId, "userLogin", userLogin, "quantity",
-									qty.negate(),"statusId","VV_WH_CHANGE_2","date", shipment.get("shipmentDate"));
+							Map serviceCtx = UtilMisc.toMap("productId", si.get("productId"), "shipmentId", shipmentId, "userLogin", userLogin, "quantity", qty.negate(), "statusId", "VV_WH_CHANGE_2",
+									"date", shipment.get("shipmentDate"));
 
 							dispatcher.runSync("createVvWh", serviceCtx);
 
@@ -292,6 +311,85 @@ public class VvorderEvents {
 		}
 		return "success";
 
+	}
+
+	public static String uploadVvOrderItem(HttpServletRequest request, HttpServletResponse response) {
+		Locale locale = UtilHttp.getLocale(request);
+
+        Map<String, Object> results = new HashMap<String, Object>();
+        ServletFileUpload fu = new ServletFileUpload(new DiskFileItemFactory(10240, new File(new File("runtime"), "tmp")));
+        List<FileItem> lst = null;
+        try {
+           lst = UtilGenerics.checkList(fu.parseRequest(request));
+        } catch (FileUploadException e4) {
+        }
+
+        if(lst.size() == 0 && UtilValidate.isNotEmpty(request.getAttribute("fileItems"))) {
+            lst = UtilGenerics.cast(request.getAttribute("fileItems"));
+        }
+        if (lst.size() == 0) {
+            return "error";
+        }
+
+
+        // This code finds the idField and the upload FileItems
+
+        FileItem fi = null;
+        FileItem imageFi = null;
+        for (int i=0; i < lst.size(); i++) {
+            fi = lst.get(i);
+            String fieldName = fi.getFieldName();
+            String fieldStr = fi.getString();
+            if (fieldName.equals("uploadFile")) {
+                imageFi = fi;
+                //MimeType of upload file
+                results.put("uploadMimeType", fi.getContentType());
+            }
+        }
+
+
+        byte[] imageBytes = imageFi.get();
+        ByteBuffer byteWrap = ByteBuffer.wrap(imageBytes);
+        results.put("imageData", byteWrap);
+        results.put("imageFileName", imageFi.getName());
+		
+		String imageName = results.get("imageFileName").toString();
+        String mimType = results.get("uploadMimeType").toString();
+        ByteBuffer imageData = (ByteBuffer) results.get("imageData");
+
+		String dirPath = "vvorder/orderitemupload/";
+		String prefix = System.getProperty("user.dir");
+		File dir = System.getProperty("user.dir") + dirPath;
+		if (!dir.exists()) {
+			boolean createDir = dir.mkdir();
+			if (!createDir) {
+				request.setAttribute("_ERROR_MESSAGE_", "Cannot create directory.");
+				return "error";
+			}
+		}
+		String imagePath = "vvorder/orderitemupload/" + imageName;
+		String prefix = System.getProperty("user.dir");
+		File file = new File(prefix + "/" + imagePath);
+		if (file.exists()) {
+			request.setAttribute("_ERROR_MESSAGE_", "There is an existing frame, please select from the existing frame.");
+			return "error";
+		}
+		Path tmpFile = Files.createTempFile(null, null);
+		Files.write(tmpFile, imageData.array(), StandardOpenOption.APPEND);
+		Files.delete(tmpFile);
+		RandomAccessFile out = new RandomAccessFile(file, "rw");
+		out.write(imageData.array());
+		out.close();
+
+
+
+
+
+
+
+
+
+		return "success";
 	}
 
 }
